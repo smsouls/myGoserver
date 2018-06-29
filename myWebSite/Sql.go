52adcf961e7268f339a7c6d6b1f9a875fb27e2ad
123 <1527340081@qq.com>
2018-06-29 10:45:33 +0800

    no message

--- myWebSite/Sql.go
+++ myWebSite/Sql.go
@@ -23,18 +23,6 @@ var createTableStatements = []string{
 		createdById VARCHAR(255) NULL,
 		PRIMARY KEY (id)
 	)`,
-
-	//"CREATE DATABASE IF NOT EXISTS library DEFAULT CHARACTER SET = 'utf8' DEFAULT COLLATE 'utf8_general_ci';",
-	//"USE library;",
-	//"CREATE TABLE IF NOT EXISTS books (id INT UNSIGNED NOT NULL AUTO_INCREMENT, " +
-	//	" title VARCHAR(255) NULL, " +
-	//	"author VARCHAR(255) NULL, " +
-	//	"publishedDate VARCHAR(255) NULL, " +
-	//	"imageUrl VARCHAR(255) NULL, " +
-	//	"description TEXT NULL, " +
-	//	"createdBy VARCHAR(255) NULL, " +
-	//	"createdById VARCHAR(255) NULL, " +
-	//	"PRIMARY KEY (id) )",
 }
 
 const (
